9599ca30431c588a48f5a99bce8fc1bec37d1b3e
Tuan Vo <automatwon@gmail.com>
2016-07-07 19:29:51 -0700

    Jade + Datastore + Routes

--- app.js
+++ app.js
@@ -13,51 +13,45 @@
 
 'use strict';
 
-var express = require('express');
 var path = require('path');
-var logger = require('morgan');
-var cookieParser = require('cookie-parser');
-var bodyParser = require('body-parser');
-
+var express = require('express');
+var config = require('./config');
 
 var app = express();
 
-// view engine setup
+app.disable('etag');
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'jade');
+app.set('trust proxy', true);
 
-app.use(logger('dev'));
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'dist')));
+app.use('/pokedex', require('./routes/pokedex.js'));
+app.use('/index', require('./routes/index.js'));
 
-// catch 404 and forward to error handler
-app.use(function (req, res, next) {
-  var err = new Error('Not Found');
-  err.status = 404;
-  next(err);
+// Redirect root to /home
+app.get('/', function (req, res) {
+  res.redirect('/index');
 });
 
+// Basic 404 handler
+app.use(function (req, res) {
+  res.status(404).send('Not Found');
+});
 
-// development error handler
-// will print stacktrace
-if (app.get('env') === 'development') {
-  app.use(function (err, req, res) {
-    res.status(err.status || 500);
-    res.render('error', {
-      message: err.message,
-      error: err
-    });
+// Basic error handler
+app.use(function (err, req, res, next) {
+  /* jshint unused:false */
+  console.error(err);
+  // If our routes specified a specific response, then send that. Otherwise,
+  // send a generic message so as not to leak anything.
+  res.status(500).send(err.response || 'Something broke!');
+});
+
+if (module === require.main) {
+  // Start the server
+  var server = app.listen(config.get('PORT'), function () {
+    var port = server.address().port;
+    console.log('App listening on port %s', port);
   });
 }
-
-// production error handler
-// no stacktraces leaked to user
-app.use(function (err, req, res) {
-  res.status(err.status || 500);
-  res.render('error', {
-    message: err.message,
-    error: {}
-  });
-});
 
 module.exports = app;
---
+++ routes/index.js
@@ -0,0 +1,60 @@
+// Copyright 2015-2016, Google, Inc.
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//    http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+'use strict';
+
+var express = require('express');
+var bodyParser = require('body-parser');
+var config = require('../config');
+
+function getModel () {
+  return require('../models/datastore');
+}
+
+var router = express.Router();
+
+// Automatically parse request body as form data
+router.use(bodyParser.urlencoded({ extended: false }));
+
+// Set Content-Type for all responses for these routes
+router.use(function (req, res, next) {
+  res.set('Content-Type', 'text/html');
+  next();
+});
+
+/**
+ * GET /books/add
+ *
+ * Display a page of books (up to ten at a time).
+ */
+router.get('/', function list (req, res, next) {
+  getModel().list(10, req.query.pageToken, function (err, entities, cursor) {
+    if (err) {
+      return next(err);
+    }
+
+    res.render('index.jade', {});
+  });
+});
+
+/**
+ * Errors on "/books/*" routes.
+ */
+router.use(function handleRpcError (err, req, res, next) {
+  // Format error and forward to generic error handler for logging and
+  // responding to the request
+  err.response = err.message;
+  next(err);
+});
+
+module.exports = router;
