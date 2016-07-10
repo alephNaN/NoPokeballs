e46f3181a511d5090d12abf4c14873b3f6f2d31c
Tuan Vo <automatwon@gmail.com>
2016-07-10 04:08:15 +0000

    Remove unused code for datastore reference in /index route

--- routes/index.js
+++ routes/index.js
@@ -3,10 +3,6 @@
 var express = require('express');
 var config = require('../config');
 
-function getModel () {
-  return require('../models/datastore');
-}
-
 var router = express.Router();
 
 // Set Content-Type for all responses for these routes
@@ -21,13 +17,7 @@ router.use(function (req, res, next) {
  * Display a page of books (up to ten at a time).
  */
 router.get('/', function list (req, res, next) {
-  getModel().list(10, req.query.pageToken, function (err, entities, cursor) {
-    if (err) {
-      return next(err);
-    }
-
-    res.render('index.jade', {});
-  });
+  res.render('index.jade', {});
 });
 
 /**
