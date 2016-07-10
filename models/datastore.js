49955f9fd3500212f9b1719a49f57b27bca48047
Tuan Vo <automatwon@gmail.com>
2016-07-10 03:55:29 +0000

    Use pokemon's number as the Pokemon entity key

--- models/datastore.js
+++ models/datastore.js
@@ -1,16 +1,3 @@
-// Copyright 2015-2016, Google, Inc.
-// Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at
-//
-//    http://www.apache.org/licenses/LICENSE-2.0
-//
-// Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and
-// limitations under the License.
-
 'use strict';
 
 var gcloud = require('gcloud');
@@ -69,6 +56,9 @@ function fromDatastore (obj) {
 //     }
 //   ]
 function toDatastore (obj, nonIndexed) {
+  
+  console.log("Before1:" + JSON.stringify(obj));
+  console.log("Before2:" + JSON.stringify(nonIndexed));
   nonIndexed = nonIndexed || [];
   var results = [];
   Object.keys(obj).forEach(function (k) {
@@ -81,6 +71,9 @@ function toDatastore (obj, nonIndexed) {
       excludeFromIndexes: nonIndexed.indexOf(k) !== -1
     });
   });
+  
+  
+  console.log("Before2:" + JSON.stringify(results));
   return results;
 }
 
@@ -124,16 +117,21 @@ function read (id, cb) {
 // data is automatically translated into Datastore format. The book will be
 // queued for background processing.
 function update (id, data, cb) {
-  var key;
-  if (id) {
-    key = ds.key([kind, parseInt(id, 10)]);
-  } else {
-    key = ds.key(kind);
-  }
-
+  // Use pokemon's number as the key
+  var key = ds.key([kind, data.number]);
+  
+  const nonIndexedFields = [
+    'description',
+    'weight',
+    'height',
+    'cost_powerup',
+    'cost_evolve',
+    'imageUrl'
+  ];
+  
   var entity = {
     key: key,
-    data: toDatastore(data, ['description'])
+    data: toDatastore(data, nonIndexedFields)
   };
 
   ds.save(
